0a7bae7a9c4511ea94eacbf4b9de8e421d94fdce
metes1 <35268717+metes1@users.noreply.github.com>
2018-02-26 04:28:55 -0500

    New function: allows user to commit and push changes.

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -29,3 +29,27 @@ find . -name "*.hs" |
     do
 	ghc -fno-code "$file" &>> Assign1/error.log
     done
+
+# Checks if any changes have been made to local repo. If yes, it gives user the option to commit and push those changes to gihub
+if cat "Assign1/changes.log" != ""
+then
+    echo "Unstaged changes within your local repo have been detected."
+    read -p "Would you like to view these changes? (Y/n) " ans1
+    if [ "$ans1" = "Y" ]
+    then
+        cat "Assign1/changes.log"
+    fi
+    read -p "Would you like to stage and commit all of these changes? (Y/n) " ans2
+    if [ "$ans2" = "Y" ]
+    then
+        echo "Please enter your commit message and then press enter to finalize commit: "
+	read message
+	git add -A
+	git commit -m "$message"
+	read -p "Would you now like to push to github? (Y/n) " ans3
+        if [ "$ans3" = "Y" ]
+	then
+	git push
+	fi
+    fi
+fi
